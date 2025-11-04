51d34382e77d87cd330074dec969d502d4e37fec
NattyEddy <99678920+NattyEddy@users.noreply.github.com>
2025-11-03 17:10:03 -0700

    Added new assignment for lab 4

---
+++ ece325_lab_assignment4/lab_assignment4/src/ece325/labs/lab4/Song.java
@@ -0,0 +1,43 @@
+package ece325.labs.lab4;
+
+/**
+ * This class represents a Song. A Song has a title and an average rating.
+ * You are not allowed to change the code that is in between the indications, but you are allowed to add
+ * code before and after the indicated lines.
+ * 
+ * @author Cor-Paul Bezemer
+ *
+ */
+public class Song ... {
+	// not allowed to change anything after this (until the next marker which says you can add code again)
+	private String title;
+	private AverageRating rating;
+	
+	public Song(String title, AverageRating rating) {
+		this.title = title;
+		this.rating = rating;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public AverageRating getAverageRating() {
+		return this.rating;
+	}
+	
+	public void setTitle(String s) {
+		title = s;
+	}
+	
+	public void setAverageRating(AverageRating rating) {
+		this.rating = rating;
+	}
+	
+	public String toString() {
+		return "[Song: " + title + ", average rating: " + rating + "]";
+	}
+	
+	// You are allowed to make changes or add code after this line
+		
+}
---
+++ ece325_lab_assignment4/lab_assignment4/src/ece325/labs/lab4/SongComparator.java
@@ -0,0 +1,3 @@
+package ece325.labs.lab4;
+
+// finish this
